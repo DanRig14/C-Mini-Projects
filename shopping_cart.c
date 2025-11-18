4a4a847e283a5352e3c5eecab6cc2b2238c1eb00
Daniel R <driggle2007@icloud.com>
2025-11-18 01:07:15 -0500

    Final Touches and Comments

--- shopping_cart.c
+++ shopping_cart.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(){
     //variables
@@ -11,11 +12,18 @@ int main(){
     //questions and user input
     printf("What item would you like to buy?: ");
     fgets(item, sizeof(item), stdin);
+    item[strlen(item) - 1] = '\0';
+
     printf("What is the price for each?: ");
     scanf("%f", &price);
+
     printf("How many are you buying?: ");
     scanf("%d", &quantity);
 
+     //calcuate total cost
+    total = price  *quantity;
+    printf("\nYou have bought %d %s/s\n", quantity, item);
+    printf("Total Price:  %c%.2f", currency, total);
 
     return 0;
 }

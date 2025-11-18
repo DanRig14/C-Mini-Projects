129e5312441b77dbdaaea9d070b565d7832ae07d
Daniel R <driggle2007@icloud.com>
2025-11-18 01:00:35 -0500

    User input

--- shopping_cart.c
+++ shopping_cart.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
 
 int main(){
-
+    //variables
     char item[50] = "";
     float price = 0.0f;
     int quantity = 0;
-    char currency = "$";
+    char currency = '$';
     float total = 0.0f;
 
-    
+    //questions and user input
+    printf("What item would you like to buy?: ");
+    fgets(item, sizeof(item), stdin);
+    printf("What is the price for each?: ");
+    scanf("%f", &price);
+    printf("How many are you buying?: ");
+    scanf("%d", &quantity);
+
+
     return 0;
 }
